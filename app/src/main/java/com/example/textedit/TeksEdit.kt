945e80b1158d9a4ac36f9a51785dbcbe09d8d2f0
Viaasyafrina16 <syafrinavia@gmail.com>
2025-10-30 08:43:32 +0700

    membuat form data diri

--- app/src/main/java/com/example/textedit/TeksEdit.kt
+++ app/src/main/java/com/example/textedit/TeksEdit.kt
@@ -1,2 +1,9 @@
 package com.example.textedit
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormDataDiri(modifier : Modifier
+){
+    //variabel-variabel untuk mengingat nilai masukan dari keyboard
+}
