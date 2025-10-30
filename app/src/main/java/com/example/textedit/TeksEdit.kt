a2c749f531fb56f71ca26b14b088565897097fca
Viaasyafrina16 <syafrinavia@gmail.com>
2025-10-30 08:57:18 +0700

    membuat column untuk input nama

--- app/src/main/java/com/example/textedit/TeksEdit.kt
+++ app/src/main/java/com/example/textedit/TeksEdit.kt
@@ -1,18 +1,47 @@
 package com.example.textedit
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier : Modifier
 ){
     //variabel-variabel untuk mengingat nilai masukan dari keyboard
-    var textNama by remember{ mutableStateOf(value = "")}
-    var textAlamat by remember{ mutableStateOf(value = "")}
-    var textJK by remember{ mutableStateOf(value = "")}
+    var textNama by remember{ mutableStateOf( "")}
+    var textAlamat by remember{ mutableStateOf( "")}
+    var textJK by remember{ mutableStateOf("")}
 
     //variabel-variabel untuk menyimpan data yang diperoleh dari komponen
-    var nama by remember {mutableStateOf(value = "")}
-    var alamat by remember {mutableStateOf(value = "")}
-    var jenis by remember {mutableStateOf(value = "")}
+    var nama by remember {mutableStateOf( "")}
+    var alamat by remember {mutableStateOf( "")}
+    var jenis by remember {mutableStateOf( "")}
+
+    val gender:List<String> = Listof("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top= 50.dp),
+        verticalArrangement = Arrangement.top,
+        horizontalAligment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(  250.dp),
+            label = { Text(text = "Nama Lengkap" )},
+            onValueChange =  {
+                textNama = it
+
+            }
+        )
+
+
+    }
 }
